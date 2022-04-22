296f74338bc9b750d9a14f2cbec063041706e7ed
Flávio Arêas <vflavio9@gmail.com>
2022-04-22 15:44:03 -0300

    feat: create card

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,44 @@
+import { motion } from 'framer-motion';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return <div className="App">Hello World</div>;
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="App">
+      <motion.div
+        className="card"
+        onClick={() => setIsOpen(!isOpen)}
+        layout
+        transition={{ duration: 1, type: 'spring' }}
+        style={{
+          borderRadius: '1rem',
+          boxShadow: '0px 10px 30px #102329',
+        }}
+      >
+        <motion.h2 layout="position">Framer Motion 🚀</motion.h2>
+        {isOpen && (
+          <motion.div
+            className="expand"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1 }}
+          >
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas
+              minima fugiat debitis voluptatem placeat consequuntur eligendi
+              obcaecati magnam dolor tempora?
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Molestiae, placeat.
+            </p>
+          </motion.div>
+        )}
+      </motion.div>
+    </div>
+  );
 }
 
 export default App;
